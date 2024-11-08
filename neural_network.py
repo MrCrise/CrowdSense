4d8e8fa301cb5a159d292bda517f0fc5155dc29d
MrCrise <andrey121205@gmail.com>
2024-11-09 00:26:02 +0500

    adafsdff

--- neural_network.py
+++ neural_network.py
@@ -2,13 +2,25 @@ from ultralytics import YOLO
 import os
 
 
+def get_picture_names():
+    names_file = open("pictures for test/picture_names.txt")
+    list_of_names = ["pictures for test\\" + i.strip() for i in names_file]
+
+
+    return list_of_names
+
+
 path = os.getcwd()
 
 
 model = YOLO("yolo11x.pt")
 
 
-results = model.predict(source="pictures for test/bus.png", save=True, classes=[0], conf=0.3, device="cuda:0", project=path)
+list_of_picture_names_for_prediction = get_picture_names()
 
 
-results[0].show
+results = model.predict(source=list_of_picture_names_for_prediction, save=True, classes=[0], conf=0.3, device="cuda:0", project=path, imgsz=1280)
+
+
+for i in results:
+    print(len(i))
